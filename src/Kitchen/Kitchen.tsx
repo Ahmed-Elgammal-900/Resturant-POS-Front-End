45abdcb989e28d2341d61aaf001bc42fab5eedfd
Ahmed-Elgammal-900 <genesis570809@gmail.com>
2025-04-29 18:48:45 +0300

    handle state

--- src/Kitchen/Kitchen.tsx
+++ src/Kitchen/Kitchen.tsx
@@ -28,7 +28,7 @@ const Kitchen = () => {
       const ordersState = ordersIDs.map(({ order_id: main }: any) =>
         orders.filter(({ order_id }: any) => order_id === main)
       );
-      console.log(JSON.stringify(ordersState));
+      console.log(JSON.stringify(ordersState, null, 2));
       const final = ordersState.map((array: any, i: number) => [
         ...array,
         ordersIDs[i],
