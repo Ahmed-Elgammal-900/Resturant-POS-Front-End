29454db64faa03374ddbb93aa2b5932372c364de
Ahmed-Elgammal-900 <genesis570809@gmail.com>
2025-04-29 03:31:55 +0300

    handle kitchen conncetion and logic

--- src/Kitchen/Kitchen.tsx
+++ src/Kitchen/Kitchen.tsx
@@ -1,5 +1,128 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBriefcase,
+  faBars,
+  faXmark,
+  faTrash,
+  faRightToBracket,
+} from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
+import { useAuth } from "../Utils/Auth";
+import { io, Socket } from "socket.io-client";
+
 const Kitchen = () => {
-    return ( <h1>Kitchen</h1> );
-}
- 
+  const [optionsPage, setOptions] = useState(false);
+  const [overlay, setOverlay] = useState(false);
+  const [socket, setSocket] = useState<Socket | null>(null);
+  const [ordersShow, setOrders] = useState<any>([]);
+  useEffect(() => {
+    const socketInstance = io(import.meta.env.VITE_API_URL, {
+      transports: ["websocket"],
+      withCredentials: true,
+    });
+
+    setSocket(socketInstance);
+    socketInstance.on("connect", () => {
+      console.log("Connected!", socketInstance.id);
+    });
+
+    socketInstance.on("ordersList", (orders, ordersIDs) => {
+      const ordersState = ordersIDs.map(({ order_id: main }: any) =>
+        orders.filter(({ order_id }: any) => order_id === main)
+      );
+
+      const final = ordersState.map((array: any, i: number) => [
+        ...array,
+        ordersIDs[i],
+      ]);
+      setOrders(final);
+    });
+
+    socketInstance.on("new order", (data, orderID) => {
+      setOrders((prev: any) => [...prev, [...data, orderID]]);
+    });
+
+    return () => {
+      socketInstance.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log(ordersShow);
+  }, [ordersShow]);
+
+  const finishOrder = (orderID: string, orderIndex: number) => {
+    if (socket) {
+      socket.emit("finishOrder", orderID);
+      setOrders((prev: any) =>
+        prev.filter((_: any, index: number) => index !== orderIndex)
+      );
+    }
+  };
+  const handleOverlay = () => {
+    setOverlay((prev) => !prev);
+  };
+  const handleOptions = () => {
+    setOptions((prev) => !prev);
+    handleOverlay();
+  };
+
+  const { Logout, deleteAccount }: any = useAuth();
+  return (
+    <>
+      <div className={overlay ? "overlay active" : "overlay"}></div>
+      <div className={optionsPage ? "options active" : "options"}>
+        <button type="button" onClick={handleOptions} className="close-options">
+          <FontAwesomeIcon icon={faXmark} />
+        </button>
+        <h1>options</h1>
+        <div className="options-button">
+          <button type="button" onClick={Logout}>
+            Logout
+            <FontAwesomeIcon icon={faRightToBracket} />
+          </button>
+          <button type="button" onClick={deleteAccount}>
+            Delete
+            <FontAwesomeIcon icon={faTrash} />
+          </button>
+        </div>
+      </div>
+      <div className="header">
+        <div className="title-dashboard">
+          <div className="icon-dashboard">
+            <FontAwesomeIcon icon={faBriefcase} color="#C8161D" size={"xl"} />
+          </div>
+          <h2>SmartPOS</h2>
+        </div>
+        <div className="dashboard-buttons">
+          <button type="button" onClick={handleOptions}>
+            <FontAwesomeIcon icon={faBars} />
+          </button>
+        </div>
+      </div>
+      <div className="orders-content">
+        {ordersShow.map((array: any, orderIndex: number) => (
+          <div className="invoice">
+            <h2>{orderIndex + 1}</h2>
+            <div className="orderinfo">
+              {array.map(({ name, count }: any) => (
+                <>
+                  <p className="name">{name}</p>
+                  <p className="count">{count}</p>
+                </>
+              ))}
+            </div>
+            <button
+              type="button"
+              onClick={() => finishOrder(array[array.length - 1], orderIndex)}
+            >
+              Done
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
 export default Kitchen;
